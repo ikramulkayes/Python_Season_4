eaf001cde1a211d2e73f51c50207596b7f1d832a
MD Ikramul Kayes <ikramul.kayesgg@gmail.com>
2022-11-17 21:11:01 +0600

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,11 @@
+def forwardprintcirculararray(lst,start,size):
+    index = start + size-1
+    index = index%len(lst)
+    i = 0
+    while i < size:
+        print(lst[index])
+        index -= 1
+        index = index% len(lst)
+        i += 1
+s = [40,50,0,0,0,0,0,0,10,20,30]
+print(forwardprintcirculararray(s,8,5))
